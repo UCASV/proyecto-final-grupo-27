6fd2adb9d89dceee74d5e2ccf710f9e04689fcc7
jonathan140 <00095520@uca.edu.sv>
2021-06-23 17:03:31 -0600

    Adding frmVaccinationProcess and frmSideEffect

--- VaccinationProject/VaccinationProject/View/frmSideEffect.cs
+++ VaccinationProject/VaccinationProject/View/frmSideEffect.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VaccinationProject
 {
-    public partial class Form1 : Form
+    public partial class frmSideEffect : Form
     {
-        public Form1()
+        public frmSideEffect()
         {
             InitializeComponent();
         }
