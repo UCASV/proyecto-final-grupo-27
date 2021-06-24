9749a78004c7fa3bc4115f1c76fd4ec1e6e75f4d
joshua1535 <00176020@uca.edu.sv>
2021-06-24 16:12:38 -0600

    Adding DBContext and created entities

---
+++ VaccinationProject/VaccinationProject/Model/Context/SideEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace VaccinationProject.Context
+{
+    public partial class SideEffect
+    {
+        public SideEffect()
+        {
+            VaccinationProcesses = new HashSet<VaccinationProcess>();
+        }
+
+        public int Id { get; set; }
+        public string SideEffect1 { get; set; }
+
+        public virtual ICollection<VaccinationProcess> VaccinationProcesses { get; set; }
+    }
+}
